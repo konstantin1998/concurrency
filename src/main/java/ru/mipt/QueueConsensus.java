4e3e0fef5ccf7936083c7cd98361506b37c44351
konstantin1998 <galperin.15@yandex.ru>
2021-05-14 12:09:21 +0300

    edit QueueConsensus

--- src/main/java/ru/mipt/QueueConsensus.java
+++ src/main/java/ru/mipt/QueueConsensus.java
@@ -1,34 +1,23 @@
 package ru.mipt;
 /*
 public class  QueueConsensus<T> extends ConsensusProtocol<T> {
-    private static final int WIN = 0;
-    private static final int LOSE = 1;
-    private static int ultimateValueIndex = -1;
-    Queue queue;
+    private Queue queue;
 
     public QueueConsensus() {
          queue = new Queue();
-         queue.enq(WIN);
-         queue.enq(LOSE);
+    }
+    protected void propose(T value) {
+        queue.enq(value);
     }
 
     public T decide(T value) {
         propose(value);
-        int status = queue.peek();
-        int i = ThreadID.get();
-        if (status == WIN) {
-            ultimateValueIndex = i;
-            queue.deq();
-        }
-        return proposed[ultimateValueIndex];
-
+        return queue.peek();
     }
 }*/
 
 /*
-Если взять за основу протокол консенсуса, описанный в учеюнике гл. 5, рисунок 5.7, с. 110 и переделать его так как
-показано выше. Тогда первый поток, входя в метод decide сможет узнать, что он первый и в поле ultimateValueIndex
-записать свой индекс как индекс, в котором лежит значение, которое должны получить все потоки. Все остальные потоки,
-пришедшие в метод  decide после первого потока, будут доставать изочереди значение LOSE и получать значение, которое
-предложил первый поток.
+Все потоки кладут свои значения в очередь, таким образом, значение, которое положил первый поток будет в начале.
+Поскольку метод peek() возврашает значение первого элемента без удаления, то все потоки будут получать значение,
+которое предложил первый поток.
 * */
