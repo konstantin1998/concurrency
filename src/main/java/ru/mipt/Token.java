c1a0abc311643ef71d55f12a3a3dd4934b835101
konstantin1998 <galperin.15@yandex.ru>
2021-06-14 17:11:15 +0300

    correct token class

--- src/main/java/ru/mipt/Token.java
+++ src/main/java/ru/mipt/Token.java
@@ -1,14 +1,4 @@
 package ru.mipt;
 
 public class Token {
-
-    private long timeStamp;
-
-    public void setTimeStamp(long timeStamp) {
-        this.timeStamp = timeStamp;
-    }
-
-    public long getTimeStamp() {
-        return timeStamp;
-    }
 }
